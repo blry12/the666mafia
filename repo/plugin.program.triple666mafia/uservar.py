894e75ba22685677620394f70d3bdc0e51345fb6
BLRY12 <bradl79@comcast.net>
2025-01-06 19:33:07 -0600

    added new wizard
    Added New Triple 666 Mafia Wizard based on Simple Wizard

---
+++ repo/plugin.program.triple666mafia/uservar.py
@@ -0,0 +1,11 @@
+'''#####-----Build File-----#####'''
+buildfile = 'https://the666mafia.com/txt/simplebuilds.txt'
+
+'''#####-----Videos File-----#####'''
+videos_url = 'http://CHANGEME'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://the666mafia.com/txt/wizard_notify.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
